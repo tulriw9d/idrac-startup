11bd7b32c6be6ca50fad275d5317207d2ca29b00
tulriw9d <35433545+tulriw9d@users.noreply.github.com>
2018-01-14 20:49:25 +0000

    Update power_restore_boot.py

--- power_restore_boot.py
+++ power_restore_boot.py
@@ -1,16 +1,16 @@
 import os
 import paramiko
 import sys
-hostname = "192.168.1.201" #SuperPi
-idrac = "192.168.1.219" #idracip
-response = os.system("ping -c 1 -w2 " + hostname + " > /dev/null 2>&1") #ping SuperPi
+hostname = "host-ip" #hostip
+idrac = "idrac-ip" #idracip
+response = os.system("ping -c 1 -w2 " + hostname + " > /dev/null 2>&1") #ping host
 #check response
 if response == 0: #host is up
-  print("SuperPi online")
+  print("Host online")
   sys.exit()
-else: #if SuperPi can't be pinged
+else: #if host can't be pinged
   response = os.system("ping -c 1 -w2 " + idrac + " > /dev/null 2>&1") #ping idrac
-  if response == 0: #host is up
+  if response == 0: #idrac is up
     ssh = paramiko.SSHClient() #ssh into idrac
     ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
     ssh.connect("192.168.1.219", port=22, username="root", password="calvin", look_for_keys=False)
