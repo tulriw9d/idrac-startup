b00ad81a587f96b6fc476dae09b1808f65cc9bf3
tulriw9d <35433545+tulriw9d@users.noreply.github.com>
2018-01-16 16:05:09 +0000

    Update power_restore_boot.py

--- power_restore_boot.py
+++ power_restore_boot.py
@@ -15,16 +15,17 @@ waittime = "WaitTimeInSeconds"
 response = os.system("ping -c 1 -w2 " + hostname + " > /dev/null 2>&1") #ping host
 #check response
 if response == 0: #host is up
-  print("Host online")
+  print('Host online')
   sys.exit()
 else: #if host can't be pinged
   response = os.system("ping -c 1 -w2 " + idrac + " > /dev/null 2>&1") #ping idrac
   if response == 0: #idrac is up
     ssh = paramiko.SSHClient() #ssh into idrac
     ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-    ssh.connect("idracipaddr", port=22, username="idracUN", password="idracPW", look_for_keys=False)
+    ssh.connect("idrac", port=22, username="idracUN", password="idracPW", look_for_keys=False)
     ssh_stdin, ssh_stdout, ssh_stderr = ssh.exec_command('racadm serveraction powerup')
     print('Starting host') 
   else: #if idrac is down
-     sys.exit() 
      print('iDrac down!')
+     sys.exit() 
+    
