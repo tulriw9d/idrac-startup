ca942d7e03e2fcfba7fc980bb2e0dff6e021c8ca
tulriw9d <35433545+tulriw9d@users.noreply.github.com>
2018-01-17 15:07:11 +0000

    Update power_restore_boot.py
    Slightly neater version

--- power_restore_boot.py
+++ power_restore_boot.py
@@ -5,27 +5,38 @@ import paramiko
 import sys
 import time
 
-hostname = "host-ip" #hostip
-idrac = "idrac-ip" #idracip
-idracUN = "youridracUN"
-idracPW = "youridracPW"
+hostname = "hostip"  # hostip
+idrac = "idracip"  # idracip
+idracUN = "YourUsername"
+idracPW = "YourPassword"
 waittime = "WaitTimeInSeconds"
 
-#time.sleep(waittime) #Uncomment if you'd like a dealy to ensure power is stable
-response = os.system("ping -c 1 -w2 " + hostname + " > /dev/null 2>&1") #ping host
-#check response
-if response == 0: #host is up
-  print('Host online')
-  sys.exit()
-else: #if host can't be pinged
-  response = os.system("ping -c 1 -w2 " + idrac + " > /dev/null 2>&1") #ping idrac
-  if response == 0: #idrac is up
-    ssh = paramiko.SSHClient() #ssh into idrac
-    ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-    ssh.connect("idrac", port=22, username="idracUN", password="idracPW", look_for_keys=False)
-    ssh_stdin, ssh_stdout, ssh_stderr = ssh.exec_command('racadm serveraction powerup')
-    print('Starting host') 
-  else: #if idrac is down
-     print('iDrac down!')
-     sys.exit() 
-    
+# ping host
+PingHOST = os.system("ping -c 1 -w2 " + hostname + " > /dev/null 2>&1")
+# ping idrac
+PingIDRAC = os.system("ping -c 1 -w2 " + idrac + " > /dev/null 2>&1")
+
+# time.sleep(waittime) #Uncomment if you'd like a dealy
+# check response
+if PingHOST == 0:  # host is up
+    print('Host online')
+    sys.exit()
+else:  # if host can't be pinged
+    if PingIDRAC == 0:  # idrac is up
+        # ssh into idrac
+        ssh = paramiko.SSHClient()
+        ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        ssh.connect(
+            idrac,
+            port=22,
+            username=idracUN,
+            password=idracPW,
+            look_for_keys=False
+            )
+        ssh_stdin, ssh_stdout, ssh_stderr = ssh.exec_command(
+            'racadm serveraction powerup'
+            )
+        print('Starting host')
+    else:  # if idrac is down
+        print('iDrac down!')
+        sys.exit()
